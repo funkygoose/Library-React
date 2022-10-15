7b7645cbdafc412f60b03a46dcbe6fddad3b549a
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-10-15 10:57:09 -0700

    initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <nav>
+      <div className="nav__container">
+        <a href="/">
+          <img src="" alt="" className="logo" />
+        </a>
+        <ul className="nav__links">
+          <li className="nav__list">
+            <a href="" className='nav__link'>
+              home
+            </a>
+          </li>
+          <li className="nav__list">
+            <a href="" className='nav__link'>
+              home
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav;
